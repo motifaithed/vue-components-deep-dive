9261938357cda0088f75069fe7f0844ad8fdcedc
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-30 06:59:40 +0800

    009 Scoped Slots

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="goalProps">
+        <h2>{{ goalProps.item }}</h2> 
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,11 +19,14 @@
 import TheHeader from './components/TheHeader.vue';
 import UserInfo from './components/UserInfo.vue';
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
 export default {
   components:{
     TheHeader,
     UserInfo,
-    BadgeList
+    BadgeList,
+    CourseGoals
   },
   data() {
     return {
---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal"></slot>
+        </li>
+    </ul>
+</template>
+<script>
+   export default {
+    data(){
+        return {
+            goals: ['Master Vue', 'Vue Lord']
+        }
+    }
+   } 
+</script>
