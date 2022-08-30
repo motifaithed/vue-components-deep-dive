b0f0e767bda65740c6c05c7d2e8e4889ef906c9b
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-30 08:07:09 +0800

    013 Teleporting Elements

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text">
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+        <teleport to="#app">
+        <error-page v-if="goalIsValid">
+            <h2>No Goal Set</h2>
+            <p>please set a goal</p>
+            <button @click="resetGoal">Okay</button>
+        </error-page>
+    </teleport>
     </div>
 </template>
+<script>
+    import ErrorPage from './ErrorPage.vue';
+    export default {
+        data(){
+            return {
+                goalIsValid: true
+            }
+        },
+        components:{
+            ErrorPage
+        },
+        methods: {
+            setGoal(){
+                
+                if(this.$refs.goal.value !== ''){
+                    console.log('hello world');
+                    this.goalIsValid = false;
+                    return;
+                }
+                console.log('test');
+                this.goalIsValid = true;
+            },
+            resetGoal(){
+                this.goalIsValid = false;
+            }
+        }
+    }
+</script>
+
